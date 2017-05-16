dff2f684a56cbf001215276319c8a699a21a3f62
Dell <mbecerracontreras@gmail.com>
2017-05-16 15:30:25 -0500

    Array sum to k with nre names

--- challenges/array_sum_k.py
+++ challenges/array_sum_k.py
@@ -1,22 +1,34 @@
 # Tienes un arreglo y un numero k. Encontrar dos numeros del arreglo que sumen k.
 
 import time
+import math
 
 k = int(raw_input())
 array = map(int, raw_input().strip().split(' '))
-
 n = len(array)
 
 start = time.time()
 
+array = sorted(i for i in array if i < k)
+
 out = False
-for i in range(n):
-	for j in range(i, n):
-		if(array[i] + array[j] == k):
-			out = True
+flag = True
+
+i = 0
+n1 = n - 1
+while(flag):
+	x = array[i] + array[n1]
+	if x == k:
+		out = True
+		break
+	elif x < k:
+		i += 1
+	else:
+		n1 -= 1
+	if n1 < math.floor(n/2) or i > math.ceil(n/2):
+		flag = False
 
 end = time.time()
 
 print out
 print(end - start)
-
